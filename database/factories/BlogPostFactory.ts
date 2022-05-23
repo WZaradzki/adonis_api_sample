7db73c02ff8a4d1cd6fb4ed003af0cab048e3ce3
Wiktor Zaradzki <mail@wiktorzaradzki.pl>
2022-05-23 14:34:33 +0200

    init

---
+++ database/factories/BlogPostFactory.ts
@@ -0,0 +1,8 @@
+import BlogPost from 'App/Models/BlogPost'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export default Factory.define(BlogPost, ({ faker }) => {
+  return {
+    //
+  }
+}).build()
